dd19d3158f8268dd354b9eac6daf8d3e42c4a05a
nadeemishikawa <nadeemishikawa@gmail.com>
2022-06-17 23:50:04 +0900

    combine both selenium and url generate

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
-
 from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import urllib
+import random
 
-def start_chrome():
+GROUP_NAME = 'セッシー'
+STUDENT_NUMBER = 'C0B20000'
+STUDENT_NAME = 'やｍだ'
+
+def generate_url(url):
+    group_name = urllib.parse.quote(GROUP_NAME)
+    student_number = urllib.parse.quote(STUDENT_NUMBER)
+    student_name = urllib.parse.quote(STUDENT_NAME)
+    place_name = urllib.parse.quote("バイト")
+
+
+    # 36度1分〜7分になる
+    morining_temp = 36 + random.randint(1, 7) / 10
+    night_temp = 36 + random.randint(1, 7) / 10
+
+    parameter = "?usp=pp_url&entry.987697424={}&entry.1451746778={}&entry.15528679={}&entry.559706162={}&entry.1257304114={}&entry.793511166={}&entry".format(
+        group_name,
+        student_number,
+        student_name,
+        morining_temp,
+        night_temp,
+        place_name
+      )
+    new_url = f'{url}?usp=pp_url&entry.987697424={group_name}&entry.1451746778={student_number}&entry.15528679={student_name}&entry.559706162={morining_temp}&entry.60078259={night_temp}&entry.793511166={place_name}&entry'
+        # "https://docs.google.com/forms/d/e/1FAIpQLSc0QOFKz0DnOO9Wx9LPX3rs3mNghKRIftLC6SNgalsTJyAF7g/viewform{}".format(parameter)
+    return new_url
+
+url = 'https://docs.google.com/forms/d/e/1FAIpQLSdELat41CE3MvmKScNKONyMSTMtRmGyQQJnOvZ49Y0ApIJ_yQ/viewform'
+new_url = generate_url(url)
+
+
+
+
+def start_chrome(url):
     driver = webdriver.Chrome()
-
-    url = "https://docs.google.com/forms/d/e/1FAIpQLSdELat41CE3MvmKScNKONyMSTMtRmGyQQJnOvZ49Y0ApIJ_yQ/viewform"
     driver.get(url)
 
     return driver
@@ -43,44 +75,15 @@ def password_google(driver):
     driver.find_element_by_name("password").send_keys(login_pw)
     time.sleep(1) # クリックされずに処理が終わるのを防ぐために追加。
     driver.find_element_by_xpath(login_pw_xpath).click()
-    time.sleep(10)
+    time.sleep(30)
     driver.quit()
 
 if __name__ == "__main__":
     # Chrome起動
-    driver = start_chrome()
+    driver = start_chrome(new_url)
 
     # Googleにログイン
     driver = login_google(driver)
 
     password_google(driver)
-
-import urllib
-import  random
-def generate_url(url):
-    group_name = urllib.parse.quote("セッシー")
-    student_number = urllib.parse.quote("C0B20000")
-    student_name = urllib.parse.quote("山田太郎")
-    place_name = urllib.parse.quote("バイト")
-
-
-    # 36度1分〜7分になる
-    morining_temp = 36 + random.randint(1, 7) / 10
-    night_temp = 36 + random.randint(1, 7) / 10
-
-    parameter = "?usp=pp_url&entry.987697424={}&entry.1451746778={}&entry.15528679={}&entry.559706162={}&entry.1257304114={}&entry.793511166={}&entry".format(
-        group_name,
-        student_number,
-        student_name,
-        morining_temp,
-        night_temp,
-        place_name
-      )
-    new_url = f'{url}?usp=pp_url&entry.987697424={group_name}&entry.1451746778={student_number}&entry.15528679={student_name}&entry.559706162={morining_temp}&entry.60078259={night_temp}&entry.793511166={place_name}&entry'
-        # "https://docs.google.com/forms/d/e/1FAIpQLSc0QOFKz0DnOO9Wx9LPX3rs3mNghKRIftLC6SNgalsTJyAF7g/viewform{}".format(parameter)
-    return new_url
-
-url = 'https://docs.google.com/forms/d/e/1FAIpQLSdELat41CE3MvmKScNKONyMSTMtRmGyQQJnOvZ49Y0ApIJ_yQ/viewform'
-new_page = generate_url(url)
-print(new_page)
 
